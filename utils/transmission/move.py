2bd7c0434e0968920e8898888bc20ce3b5d827e7
ChronoDi <s_bubnov@hotmail.com>
2023-06-27 15:03:38 +0300

    v.2.0.0.1

--- utils/transmission/move.py
+++ utils/transmission/move.py
@@ -4,6 +4,6 @@ from utils.transmission import client
 from config_data import config
 
 
-def move_torrent(torrent: Torrent, path: str) -> None:
+def move_torrent(torrent_id: int, path: str) -> None:
     download_dir: str = f'{config.transmission.download_folder}{path}'
-    client.move_torrent_data(torrent.id, location=download_dir)
+    client.move_torrent_data(torrent_id, location=download_dir)
